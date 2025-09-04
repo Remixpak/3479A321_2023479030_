33bc7ed2558d8e7a0d3ee686a95eb3c2ec54c0f7
88kay <samuelferle123@gmail.com>
2025-09-04 09:14:47 -0400

    ahora si

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -3,14 +3,7 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  
 
   final String title;
 
@@ -27,14 +20,14 @@ class _MyHomePageState extends State<MyHomePage> {
   ) //ignorar, esto lo que hacia era cambiar en base al numero del contador
   {
     setState(() {
-      _currentColor = newColor; //actualiza el color actual
+      _currentColor = newColor;//actualiza el color actual
     });
   }
 
   void changeColor2() {
     setState(() {
       if (_currentColor ==
-          const Color.fromARGB(255, 255, 255, 255)) //color base
+          const Color.fromARGB(255, 255, 255, 255))//color base
       {
         _currentColor = const Color.fromARGB(
           255,
@@ -43,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
           233,
         ); //cambia a morado
       } else if (_currentColor ==
-          const Color.fromARGB(255, 118, 68, 233)) //morado
+          const Color.fromARGB(255, 118, 68, 233))//morado
       {
         _currentColor = const Color.fromARGB(
           255,
