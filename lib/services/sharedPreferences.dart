dd96992bc35c4056e5c8c3f4fe1f63058a1c9ce1
Remixpak <xxremixpakxx@gmail.com>
2025-10-15 00:08:49 -0300

    lab 6 a medio terminar

---
+++ lib/services/sharedPreferences.dart
@@ -0,0 +1,28 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_native_type_patch.dart';
+
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesService {
+  String KeySize = 'size';
+  String KeyPalette = 'palette';
+
+  Future<void> saveSize(int size) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt(KeySize, size);
+  }
+
+  Future<int> LoadSize() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getInt(KeySize) ?? 16;
+  }
+
+  Future<void> savePalette(String palette) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(KeyPalette, palette);
+  }
+
+  Future<String> LoadPalette() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(KeyPalette) ?? 'default';
+  }
+}
